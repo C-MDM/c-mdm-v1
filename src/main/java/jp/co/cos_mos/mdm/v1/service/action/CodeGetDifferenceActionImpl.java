b9d7aab3e0b753c0fc724ede176a33860053f50a
cosmos <cosmos@P10119>
2015-01-30 14:58:02 +0900

    クラス名を正しいものに修正

--- src/main/java/jp/co/cos_mos/mdm/v1/service/action/CodeGetDifferenceActionImpl.java
+++ src/main/java/jp/co/cos_mos/mdm/v1/service/action/CodeGetDifferenceActionImpl.java
@@ -18,7 +18,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
-public class CodeGetDifferenceImpl implements CodeGetDifference {
+public class CodeGetDifferenceActionImpl implements CodeGetDifferenceAction {
 
 	@Autowired
 	private CodeMapper codeMapper;
